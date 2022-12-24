32f9ce8ae4ed562706acfd95bf2e2bc5221acb24
Duvall Pinkney <14938639+Dpinkney001@users.noreply.github.com>
2022-12-24 17:38:00 -0500

    Add files via upload

---
+++ monster cards.h
@@ -0,0 +1,37 @@
+/// testing the creation of monster cards
+
+// enum monster types.
+
+enum monsterType {
+	1 == FIEND,
+	2 == SPELLCASTER,
+	3 == ROCK,
+	4 == MACHINE,
+	5 == ZOMBIE,
+	6 == DRAGON,
+	7 == 
+
+
+
+	// enum monster attribute
+
+	enum monsterAttribute {
+		1 == DARK,
+		2 == LIGHT,
+		3 == WIND,
+		4 == EARTH,
+		5 == FIRE,
+		6 == WATER,
+		7 == DIVINE,
+
+ int Dark_Necrofear(){
+	int stars = 8;
+	int atk = 2200;
+	int def = 2800;
+	int monsterType = FIEND;
+	int monsterAttribute = DARK;
+	int effectMonster = true;
+	int normalMonster = false;
+	// effect
+
+	if ( graveyardFiendCount => 3 ){
